54dd4b6e1c068577825fc81d76ff3bb3aaeae195
ezntek <eason@ezntek.com>
2024-06-28 13:01:24 +0800

    sync slider and label, start libtpfanspeed

--- tpfanspeed/src/components/fancontrol.rs
+++ tpfanspeed/src/components/fancontrol.rs
@@ -1,3 +1,4 @@
+use glib::clone;
 use gtk::prelude::*;
 
 pub struct FancontrolFrame {}
@@ -22,6 +23,7 @@ impl FancontrolFrame {
 
         let auto_radiobutton = gtk::CheckButton::builder()
             .label("Auto (Embedded Controller Adjusts)")
+            .active(true)
             .build();
         let fullspeed_radiobutton = gtk::CheckButton::builder()
             .group(&auto_radiobutton)
@@ -43,15 +45,20 @@ impl FancontrolFrame {
             .margin_end(5)
             .build();
 
-        let adj = gtk::Adjustment::new(0.0, 0.0, 7.0, 1.0, 1.0, 1.0);
+        let label = gtk::Label::new(Some("0"));
+        let adj = gtk::Adjustment::new(0.0, 0.0, 8.0, 1.0, 1.0, 1.0);
         let slider = gtk::Scale::builder()
             .orientation(gtk::Orientation::Horizontal)
             .adjustment(&adj)
             .hexpand(true)
             .build();
 
+        slider.connect_value_changed(clone!(@weak slider, @weak label => move |_| {
+            label.set_text(&format!("{:.0}", slider.value()));
+        }));
+
         slider_hbox.append(&slider);
-        slider_hbox.append(&gtk::Label::new(Some("4")));
+        slider_hbox.append(&label);
 
         vbox.append(&auto_radiobutton);
         vbox.append(&fullspeed_radiobutton);
